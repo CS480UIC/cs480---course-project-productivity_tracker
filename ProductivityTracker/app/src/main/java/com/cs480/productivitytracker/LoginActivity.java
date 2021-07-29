362dc99529b4fc8974b51ab9eeda269edc877293
Jacob Diaz <hello@jacobdiaz.dev>
2021-07-29 18:35:03 -0500

    (Jacob) feat: login screen implemented

--- ProductivityTracker/app/src/main/java/com/cs480/productivitytracker/LoginActivity.java
+++ ProductivityTracker/app/src/main/java/com/cs480/productivitytracker/LoginActivity.java
@@ -2,13 +2,46 @@ package com.cs480.productivitytracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.cs480.databaseAPI.*;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    String userNameValue, passwordValue;
+    EditText userNameInput,passwordInput;
+    Button signInBtn;
+
+
+    public static String MySqlDatabase = "ProductivityTracker";
+    public static String MySqlUser = "root";
+    public static String MySqlPassword = "capnsdelight12";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide(); // hide the title bar
+        setContentView(R.layout.login_activity);
+
+        // Form UI
+        userNameInput = (EditText) findViewById(R.id.username_input);
+        passwordInput = (EditText) findViewById(R.id.password_input);
+        signInBtn = (Button) findViewById(R.id.sign_in_btn);
+    }
+    public void handleSignIn(View view) {
+        // Grab field values
+        userNameValue = userNameInput.getText().toString();
+        passwordValue = passwordInput.getText().toString();
+
+        if (UserAPI.verifyUser(userNameValue, passwordValue)) {
+            Log.i("Login Activity", "Inputs: "+userNameValue+" | "+passwordValue);
+        } else {
+            Log.i("Login Activity", "Error");
+        }
     }
 }
--- ProductivityTracker/app/src/main/java/com/cs480/productivitytracker/MainActivity.java
+++ ProductivityTracker/app/src/main/java/com/cs480/productivitytracker/MainActivity.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); // hide the title bar
         setContentView(R.layout.activity_main);
     }
+
 }
