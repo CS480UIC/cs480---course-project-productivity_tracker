caf555055417181de7da03694477a1579b56b91b
yashkurkure <yash.kurkure1903@gmail.com>
2021-07-29 19:27:18 +0530

    (Yash) Added android app skeleton

---
+++ ProductivityTracker/app/src/main/java/com/cs480/databaseAPI/App.java
@@ -0,0 +1,10 @@
+package com.cs480.databaseAPI;
+
+public class App
+{
+    public static String MySqlDatabase = "ProductivityTracker";
+    public static String MySqlUser = "root";
+    public static String MySqlPassword = "capnsdelight12";
+
+    public static String LoggedInUser;
+}
---
+++ ProductivityTracker/app/src/main/java/com/cs480/productivitytracker/DashboardActivity.java
@@ -0,0 +1,4 @@
+package com.cs480.productivitytracker;
+
+public class DashboardActivity {
+}
