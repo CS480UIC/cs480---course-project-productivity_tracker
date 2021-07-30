1b7ae8f96e6b126272b2a92991c2bebdc71f41be
Jake Diaz <jakediaz@c02f6ds8md6m.attlocal.net>
2021-07-30 00:50:08 -0500

    created dashboard-ui

--- ProductivityTracker/app/src/main/java/com/cs480/productivitytracker/DashboardActivity.java
+++ ProductivityTracker/app/src/main/java/com/cs480/productivitytracker/DashboardActivity.java
@@ -1,4 +1,15 @@
 package com.cs480.productivitytracker;
 
-public class DashboardActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); // hide the title bar
+        setContentView(R.layout.dashboard_activity);
+    }
 }
